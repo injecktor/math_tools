5bebfc89267f66b66d5b5db1273534cb538113c2
InJeckt <p1o1l1i1e1x1o1v@mail.ru>
2025-05-09 20:08:57 +0500

    added fast sort

--- sorts.hpp
+++ sorts.hpp
@@ -1,15 +1,14 @@
 namespace math_tools {
     template<class T>
     void bubble_sort(T* start, T* end) {
+        if (start >= end) return;
         T* max_ptr = end;
         while (max_ptr != start) {
             T* max_ptr_itr = start;
             for (T* i = start + 1; i <= max_ptr; i++) {
                 T* prev = i - 1;
                 if (*prev > *i) {
-                    T tmp = *prev;
-                    *prev = *i;
-                    *i = tmp;
+                    math_tools::swap(prev, i);
                     max_ptr_itr = i;
                 }
             }
@@ -19,6 +18,7 @@ namespace math_tools {
 
     template<class T>
     void selection_sort(T* start, T* end) {
+        if (start >= end) return;
         for (T* i = start; i < end; i++) {
             T *min = i;
             for (T* j = i + 1; j <= end; j++) {
@@ -27,30 +27,44 @@ namespace math_tools {
                 }
             }
             if (min != i) {
-                T tmp = *i;
-                *i = *min;
-                *min = tmp;
+                math_tools::swap(i, min);
             }
         }
     }
 
     template<class T>
     void insertion_sort(T* start, T* end) {
+        if (start >= end) return;
         for (T* i = start + 1; i <= end; i++) {
             T* last_sorted = i - 1;
             while (last_sorted >= start && *last_sorted > *(last_sorted + 1)) {
-                T tmp = *last_sorted;
-                *last_sorted = *(last_sorted + 1);
-                *(last_sorted + 1) = tmp;
+                math_tools::swap(last_sorted, last_sorted + 1);
                 last_sorted--;
             }
         }
     }
 
-    template<class T>
-    void fast_sort(T* start, T* end) {
-        
+    template<class T, size_t T_size = sizeof(T)>
+    static T* fast_sort_part(T* start, T* end) {
+        T *pivot = (end - start) / 2 / T_size * T_size;
+        while (start <= end) {
+            while (*start < *pivot) start++;
+            while (*end > *pivot) end--;
+            if (start <= end) {
+                math_tools::swap(start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
     }
 
+    template<class T>
+    void fast_sort(T* start, T* end) {
+        if (start >= end) return;
+        T* new_start = fast_sort_part(start, end);
+        fast_sort(start, new_start - 1);
+        fast_sort(new_start, end);
+    }
     
 }
