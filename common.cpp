549114964f676dffdc7c784ee2474b2702e93e57
injeckt <p1o1l1i1e1x1o1v@mail.ru>
2025-05-19 18:14:23 +0500

    added endian functions

---
+++ common.cpp
@@ -0,0 +1,63 @@
+#include "common.hpp"
+
+math_tools::endianness_t host = math_tools::endianness_t::unknown;
+
+inline endianness_t math_tools::get_host_endian() {
+    if (host == math_tools::endianness_t::unknown) {
+        math_tools::endianness_t tmp;
+        int a = 1;
+        if (*reinterpret_cast<unsigned char*>(&a) == 1) {
+            tmp = math_tools::endianness_t::little;
+        } else {
+            tmp = math_tools::endianness_t::big;
+        }
+        host = tmp;
+    }
+    return host;
+}
+
+short math_tools::htobe16(short data) {
+    short result = data;
+    if (host == math_tools::endianness_t::unknown) {
+        host = math_tools::get_host_endian();
+    }
+    if (host != math_tools::endianness_t::big) {
+        result = static_cast<unsigned short>(data) << 8 | data >> 8;
+    }
+    return result;
+}
+
+short math_tools::htole16(short data) {
+    short result = data;
+    if (host == math_tools::endianness_t::unknown) {
+        host = math_tools::get_host_endian();
+    }
+    if (host != math_tools::endianness_t::little) {
+        result = static_cast<unsigned short>(data) << 8 | data >> 8;
+    }
+    return result;
+}
+
+int math_tools::htobe32(int data) {
+    int result = data;
+    if (host == math_tools::endianness_t::unknown) {
+        host = math_tools::get_host_endian();
+    }
+    if (host != math_tools::endianness_t::big) {
+        auto ptr = reinterpret_cast<unsigned char*>(&data);
+        result = ptr[0] << 24 | ptr[1] << 16 | ptr[2] << 8 | ptr[3];
+    }
+    return result;
+}
+
+int math_tools::htole32(int data) {
+    int result = data;
+    if (host == math_tools::endianness_t::unknown) {
+        host = math_tools::get_host_endian();
+    }
+    if (host != math_tools::endianness_t::little) {
+        auto ptr = reinterpret_cast<unsigned char*>(&data);
+        result = ptr[3] << 24 | ptr[2] << 16 | ptr[1] << 8 | ptr[0];
+    }
+    return result;
+}
--- common.hpp
+++ common.hpp
@@ -1,15 +1,27 @@
 namespace math_tools {
 
-template<class T>
-T border(T value, T min, T max) {
-    return value > max ? max : (value < min ? min : value);
-}
+    enum endianness_t {
+        unknown,
+        little,
+        big
+    };
 
-template<class T>
-void swap(T* left, T* right) {
-    T tmp = *left;
-    *left = *right;
-    *right = tmp;
-}
+    extern inline endianness_t get_host_endian();
+    extern short htobe16(short data);
+    extern short htole16(short data);
+    extern int htobe32(int data);
+    extern int htole32(int data);
+
+    template<class T>
+    T border(T value, T min, T max) {
+        return value > max ? max : (value < min ? min : value);
+    }
+
+    template<class T>
+    void swap(T* left, T* right) {
+        T tmp = *left;
+        *left = *right;
+        *right = tmp;
+    }
 
 }
